9b79125311a94559623b10cf3dec313aa238035b
clevandangkhoa <67139427+clevandangkhoa@users.noreply.github.com>
2020-09-10 17:04:55 +0700

    Hệ đếm

--- bai2.pas
+++ bai2.pas
@@ -1,8 +1,23 @@
 uses crt;
 var i:byte;
     f:text;
-    function nto(n:integer):boolean;
-    var i:integer;
+    function nhiphan(n:integer):boolean;
+    var i,j,code:integer;
+        tam,s:string;
+    begin
+         nhiphan:=false;
+         while n<>0 do
+         begin
+              str(n mod 2,tam);
+              s:=tam+s;
+              n:=n div 2;
+         end;
+         for i:=1 to (length(s) div 2) do
+             if s[i]<>s[length(s)-i+1] then exit;
+         nhiphan:=true;
+    end;
+    function nto(n:longint):boolean;
+    var i:longint;
     begin
          nto:=false;
          for i:=2 to n div 2 do
@@ -11,7 +26,8 @@ var i:byte;
     end;
 begin
      clrscr;
-     for i:=0 to 99 do
-         if nto(i*10+(i div 10))=true then write(i*10+(i div 10),' ');
+     nhiphan(727);
+     for i:=10 to 99 do
+         if (nto(i*10+(i div 10))=true) and (nhiphan(i*10+(i div 10))=true) then write(i*10+(i div 10),' ');
      readln;
 end.
